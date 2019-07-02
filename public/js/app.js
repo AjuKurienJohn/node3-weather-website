4b551d0c8ef231fdbc03030782bd50b4778a688f
AJ John <ajukjohn@gmail.com>
2019-07-02 15:53:19 -0400

    change image name to uppercase

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,8 @@ weatherForm.addEventListener('submit', (e) => {
                 messageOne.textContent = data.error
             else {
                 messageOne.textContent = data.location
-                messageTwo.textContent = data.forecast
+                //messageTwo.textContent = data.forecast
+                messageTwo.innerHTML = data.forecast
             }
         })
     })
